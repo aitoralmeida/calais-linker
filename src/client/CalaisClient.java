7c8b382d146d93b6b270046690e4216e76a9371a
Aitor Almeida <aitoralmeida@gmail.com>
2013-02-25 16:15:56 +0100

    added method to write the result to a file

--- src/client/CalaisClient.java
+++ src/client/CalaisClient.java
@@ -1,6 +1,9 @@
 package client;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URLEncoder;
 
 import org.restlet.data.Form;
@@ -9,13 +12,13 @@ import org.restlet.resource.ClientResource;
 
 public class CalaisClient {
 	
-	String TEST_TEXT = "This is a test, my name is Aitor Almeida and I'm in Bilbao, Spain. I'm writing a longer text because the previous one was too short.";
+	String TEST_TEXT = "This is a test, my name is Aitor Almeida and I'm in Bilbao, Spain. I'm writing a longer text because the previous one was too short. London, IBM";
 	
 	public void getAnalysis() throws IOException{
 		Form form = new Form();
 		form.add("licenseID", URLEncoder.encode(ConfigValues.KEY, "UTF-8"));
 		form.add("content", URLEncoder.encode(this.TEST_TEXT, "UTF-8"));
-		//form.add("paramsXML",URLEncoder.encode(this.getParamsXml(), "UTF-8"));
+		//form.add("paramsXML",URLEncoder.encode(ConfigValues.getParameters(), "UTF-8"));
 		
 		ClientResource resource = new ClientResource(ConfigValues.CALAIS_URL);
 
@@ -23,12 +26,33 @@ public class CalaisClient {
 
 		if (resource.getStatus().isSuccess()) {
 			System.out.println("Success");
-			System.out.println(r.getText());
+			
 		} else {
 			System.out.println("Fail");
-			System.out.println(r.getText());
 		}
+		String response = r.getText();
+		System.out.println(response);
+		this.writeContentToFile(response);
 	}
 	
+	
+	private void writeContentToFile(String content){
+		String path = "./output/outputContet.rdf";
+		File file = new File(path);
+		if (file.exists()){
+			file.delete();
+		}
+		FileWriter outFile;
+		try {
+			outFile = new FileWriter(path);
+			PrintWriter out = new PrintWriter(outFile);
+			out.print(content);
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 }
