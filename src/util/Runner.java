b92122d1bf89dfc493b5639659c8ee9a8a8c9b01
Aitor Almeida <aitoralmeida@gmail.com>
2013-02-26 11:51:35 +0100

    added test for CalaisClient.getAnalysis

--- src/util/Runner.java
+++ src/util/Runner.java
@@ -9,7 +9,7 @@ public class Runner {
 	public static void main(String[] args) {
 		CalaisClient client = new CalaisClient();
 		try {
-			client.getAnalysis();
+			client.getAnalysis(CalaisClient.TEST_TEXT);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
---
+++ test/client/CalaisClientTest.java
@@ -0,0 +1,18 @@
+package client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalaisClientTest {
+
+	@Test
+	public void testGetAnalysis() throws Exception{
+		String test = "This is a test, I'm in Bilbao, Spain. I'm writing a longer text because the previous one was too short. Test Test Test Test Test Test Test Test ";
+		String expected = "<c:name>Bilbao,Biscay,Spain</c:name><c:shortname>Bilbao</c:shortname><c:containedbystate>Biscay</c:containedbystate><c:containedbycountry>Spain</c:containedbycountry><c:latitude>43.25</c:latitude><c:longitude>-2.9667</c:longitude>";
+		CalaisClient client = new CalaisClient();
+		String result = client.getAnalysis(test);
+		assertTrue(result.contains(expected));
+	}
+
+}
