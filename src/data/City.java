58291f85fe9960b189e7691d9b27d7c035a209c1
Aitor Almeida <aitoralmeida@gmail.com>
2013-02-27 16:18:30 +0100

    DAO for the city

---
+++ src/data/City.java
@@ -0,0 +1,62 @@
+package data;
+
+public class City {
+	private String name;
+	private String shortname;
+	private String country;
+	private String latitude;
+	private String longitude;
+	
+	public City(String name, String shortname, String country, String latitude,
+			String longitude) {
+		super();
+		this.name = name;
+		this.shortname = shortname;
+		this.country = country;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShortname() {
+		return shortname;
+	}
+
+	public void setShortname(String shortname) {
+		this.shortname = shortname;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+
+}
