f15976f99500793e97e19115f98a1353dd6b7e44
Aitor Almeida <aitoralmeida@gmail.com>
2013-02-27 11:10:25 +0100

    checks for the correct HTTP response code

--- src/client/CalaisClient.java
+++ src/client/CalaisClient.java
@@ -12,12 +12,12 @@ import org.restlet.resource.ClientResource;
 
 public class CalaisClient {
 	
-	String TEST_TEXT = "This is a test, my name is Aitor Almeida and I'm in Bilbao, Spain. I'm writing a longer text because the previous one was too short. London, IBM";
+	public static String TEST_TEXT = "This is a test, my name is Aitor Almeida and I'm in Bilbao, Spain. I'm writing a longer text because the previous one was too short. London, IBM";
 	
-	public void getAnalysis() throws IOException{
+	public String getAnalysis(String content) throws IOException{
 		Form form = new Form();
 		form.add("licenseID", URLEncoder.encode(ConfigValues.KEY, "UTF-8"));
-		form.add("content", URLEncoder.encode(this.TEST_TEXT, "UTF-8"));
+		form.add("content", URLEncoder.encode(content, "UTF-8"));
 		//form.add("paramsXML",URLEncoder.encode(ConfigValues.getParameters(), "UTF-8"));
 		
 		ClientResource resource = new ClientResource(ConfigValues.CALAIS_URL);
@@ -25,19 +25,27 @@ public class CalaisClient {
 		Representation r = resource.post(form.getWebRepresentation());
 
 		if (resource.getStatus().isSuccess()) {
-			System.out.println("Success");
+			System.out.println("Success: " + resource.getStatus().getCode());
+			if (resource.getStatus().getCode() == 200){
+				String response = r.getText();
+				this.processResponse(response);
+			}
 			
 		} else {
 			System.out.println("Fail");
 		}
+		
 		String response = r.getText();
-		System.out.println(response);
-		this.writeContentToFile(response);
+		return response;
 	}
 	
+	private void processResponse(String content){
+		System.out.println(content);
+		this.writeContentToFile(content);		
+	}
 	
 	private void writeContentToFile(String content){
-		String path = "./output/outputContet.rdf";
+		String path = "./output/test/outputContent.rdf";
 		File file = new File(path);
 		if (file.exists()){
 			file.delete();
