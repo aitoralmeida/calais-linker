bbffaa1d1f2a1d127ec6bd7cac12012e381a18ea
Aitor Almeida <aitoralmeida@gmail.com>
2013-02-27 12:35:43 +0100

    renamed, now only contains config values for the calais client

---
+++ src/client/ConfigValuesCalais.java
@@ -0,0 +1,11 @@
+package client;
+
+public class ConfigValuesCalais {
+
+	public static String KEY = "YOUR_KEY_HERE"; //Replace this with the API key
+	public static String CALAIS_URL = "http://api1.opencalais.com/enlighten/rest/";
+	public static String CONTENT_TYPE = "TEXT/RAW";
+	public static String OUTPUT_FORMAT = "XML/RDF";
+	public static String SUBMITTER ="Calais-linker";
+
+}
