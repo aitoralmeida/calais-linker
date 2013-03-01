ae0ca2fca83472739b52c6a62e8a879d3efa1110
Aitor Almeida <aitoralmeida@gmail.com>
2013-03-01 11:31:57 +0100

    recovering the dbpedia resource for cities

--- src/client/DBPediaClient.java
+++ src/client/DBPediaClient.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,25 +12,42 @@ import org.restlet.resource.ClientResource;
 public class DBPediaClient {
 	
 	public static String DBPEDIA_URL_SEARCH = "http://dbpedia.org/sparql?query=";
+	
+	//Queries for companies
 	public static String COMPANY_QUERY_START_NAME = "select ?uri where {?uri dbpprop:name \"";
 	public static String COMPANY_QUERY_START_SHORTNAME = "select ?uri where {?uri  rdfs:label \"";
 	public static String COMPANY_QUERY_START_SYMBOL = "select ?uri where {?uri dbpprop:symbol \"";
 	public static String COMPANY_QUERY_END = "\"@en . ?uri rdf:type dbpedia-owl:Company}";
 	
-	public String searchResource(String companyName, String companyShortname, String companySymbol) throws IOException{
+	//Queries for places
+	public static String CITY_QUERY_START = "select ?uri where {?uri dbpprop:name \"";
+	public static String CITY_QUERY_MIDDLE = "\"@en . ?uri rdf:type dbpedia-owl:Place .?uri dbpedia-owl:country dbpedia:";
+	public static String CITY_QUERY_END = " .}";
+	
+	
+	public String searchCityResource(String cityName, String country) throws IOException{
+		String modifiedCounty = country.replace(" ", "_");
+		String query = DBPediaClient.CITY_QUERY_START + cityName + DBPediaClient.CITY_QUERY_MIDDLE + modifiedCounty + DBPediaClient.CITY_QUERY_END;
+		String url = DBPediaClient.DBPEDIA_URL_SEARCH;
+		url = url + URLEncoder.encode(query, "UTF-8");
+
+		String uri = executeQuery(url);
+		return uri;
+	}
+	
+	public String searchCompanyResource(String companyName, String companyShortname, String companySymbol) throws IOException{
 		String uri= "";
-		uri = this.searchBySymbol(companySymbol);
+		uri = this.searchCompanyBySymbol(companySymbol);
 		if (uri.isEmpty()){
-			uri = this.searchByName(companyName);
+			uri = this.searchCompanyByName(companyName);
 			if (uri.isEmpty()){
-				uri = this.searchByShortname(companyShortname);
+				uri = this.searchCompanyByShortname(companyShortname);
 			}
 		}
 		return uri;		
 	}
 
-
-	private String searchByName(String companyName) throws IOException{
+	private String searchCompanyByName(String companyName) throws IOException{
 		String url = DBPediaClient.DBPEDIA_URL_SEARCH;
 		String query = DBPediaClient.COMPANY_QUERY_START_NAME + companyName + DBPediaClient.COMPANY_QUERY_END;
 		url = url + URLEncoder.encode(query, "UTF-8");
@@ -37,7 +55,7 @@ public class DBPediaClient {
 		return uri;
 	}
 	
-	private String searchByShortname(String companyShortname) throws IOException{
+	private String searchCompanyByShortname(String companyShortname) throws IOException{
 		String url = DBPediaClient.DBPEDIA_URL_SEARCH;
 		String query = DBPediaClient.COMPANY_QUERY_START_SHORTNAME + companyShortname + DBPediaClient.COMPANY_QUERY_END;
 		url = url + URLEncoder.encode(query, "UTF-8");
@@ -45,7 +63,7 @@ public class DBPediaClient {
 		return uri;
 	}
 	
-	private String searchBySymbol(String companySymbol) throws IOException{
+	private String searchCompanyBySymbol(String companySymbol) throws IOException{
 		String url = DBPediaClient.DBPEDIA_URL_SEARCH;
 		String query = DBPediaClient.COMPANY_QUERY_START_SYMBOL + companySymbol + DBPediaClient.COMPANY_QUERY_END;
 		url = url + URLEncoder.encode(query, "UTF-8");
@@ -60,8 +78,6 @@ public class DBPediaClient {
 		String uri = this.getUri(result);
 		return uri;
 	}
-	
-	
 	
 	String getUri(String result){
 		//Pattern pattern = Pattern.compile("\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
--- test/client/DBPediaClientTest.java
+++ test/client/DBPediaClientTest.java
@@ -7,16 +7,26 @@ import org.junit.Test;
 public class DBPediaClientTest {
 
 	@Test
-	public void testSearch() throws Exception{
+	public void testSearchCompanies() throws Exception{
 		DBPediaClient client = new DBPediaClient();
-		String result = client.searchResource("INTERNATIONAL BUSINESS MACHINES CORPORATION", "IBM", "IBM");
+		String result = client.searchCompanyResource("INTERNATIONAL BUSINESS MACHINES CORPORATION", "IBM", "IBM");
 		assertEquals("http://dbpedia.org/resource/IBM", result);
 		
-		result = client.searchResource("Google Inc", "Google", "GOOG");
+		result = client.searchCompanyResource("Google Inc", "Google", "GOOG");
 		assertEquals("http://dbpedia.org/resource/Google", result);
 		
-		result = client.searchResource("Apple Inc.", "Apple", "AAPL");
+		result = client.searchCompanyResource("Apple Inc.", "Apple", "AAPL");
 		assertEquals("http://dbpedia.org/resource/Apple_Inc", result);		
+	}
+	
+	@Test
+	public void testSearchCities() throws Exception{
+		DBPediaClient client = new DBPediaClient();
+		String result = client.searchCityResource("Bilbao", "Spain");
+		assertEquals("http://dbpedia.org/resource/Bilbao", result);
+		
+		result = client.searchCityResource("London", "United Kingdom");
+		assertEquals("http://dbpedia.org/resource/London", result);
 	}
 	
 	
